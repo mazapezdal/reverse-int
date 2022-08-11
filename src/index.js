81fabfc898073c0c7674af06102a3845accc024e
Vlad <105743454+mazapezdal@users.noreply.github.com>
2022-08-11 07:35:59 +0300

    Update index.js
    fear: add "-"

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 module.exports = function reverse (n) {
-   
+   if(n>0) {
   return +n.toString().split("").reverse().join("");
+   }
+    else{
+        return (+(n*(-1)).toString().split("").reverse().join("")) * (-1);
+    }
 }
