eabb998d0809b68d202bfe3b6daaa9d19a5c9147
Vlad <105743454+mazapezdal@users.noreply.github.com>
2022-08-11 07:50:43 +0300

    Update index.js
    feat: #5

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 module.exports = function reverse (n) {
    if(n>0) {
   return +n.toString().split("").reverse().join("");
-   };
-    else{
+   } else{
         return +((n*(-1)).toString().split("").reverse().join(""));
     };
 }
